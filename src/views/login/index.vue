456f521fa69831b8a3f7200f358d55fd8cd2a5c2
XULONG LIU <xaskleowhyl@163.com>
2021-10-26 17:00:27 +0800

    feat: 登录页默认用户名密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -87,8 +87,8 @@ export default {
     }
     return {
       loginForm: {
-        username: '',
-        password: ''
+        username: 'admin',
+        password: '123456'
       },
       loginRules: {
         username: [
